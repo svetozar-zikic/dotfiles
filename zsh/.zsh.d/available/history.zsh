34cd75f582013b2d9eb15e0fb9ef0babd31a8ae8
Svetozar Žikić <svetozar.zikic@gmail.com>
2020-11-23 11:37:55 -0800

    Add reverse lookup, tidy up

--- zsh/.zsh.d/available/history.zsh
+++ zsh/.zsh.d/available/history.zsh
@@ -3,7 +3,12 @@ zinit snippet PZT::modules/history/init.zsh
 zinit snippet "https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh"
 zinit snippet "https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh"
 
-# shell specific commands
-HISTFILE=~/.zhistory
-HISTCONTROL=ignorespace             # shell commands that start with space will NOT be added to history
+# enable reverse lookup
+bindkey '^R' history-incremental-search-backward
+
+# don't save commands that start with space
+HISTCONTROL=ignorespace
+
+# env
 HISTSIZE=10000
+HISTFILE=~/.zhistory
