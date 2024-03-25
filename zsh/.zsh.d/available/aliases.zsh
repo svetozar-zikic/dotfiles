79df1022be20c80a0e09d41690999b84a5635223
Svetozar Žikić <svet.zikic@15five.com>
2024-03-24 20:21:51 -0400

    Update aliases and gitignore

--- zsh/.zsh.d/available/aliases.zsh
+++ zsh/.zsh.d/available/aliases.zsh
@@ -2,6 +2,7 @@ alias ga='git add '
 alias gb='git branch '
 alias gc='git commit'
 alias gco='git checkout '
+alias gpp='git pull --prune'
 alias gd='git diff '
 alias gs='git status '
 alias grv='git remote -v'
@@ -10,7 +11,8 @@ alias ll='ls -l'
 alias s="systemctl"
 alias sc="ktx"
 alias sn="kns"
-alias t='terraform'
+alias tf='terraform'
+alias tg='terragrunt'
 #alias vim='nvim'
 
 if [[ "$OSTYPE" == "darwin"*  ]]; then alias ls='ls -G'; else alias ls='ls --color=auto'; fi
